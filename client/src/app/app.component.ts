fa6036049ee9a2364f269353d1dd634af383aa95
Regina Ward <regina.ward94@outlook.com>
2023-05-16 01:52:11 -0500

    created navbar with bootstrap

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { setTheme } from 'ngx-bootstrap/utils';
 
 @Component({
   selector: 'app-root',
@@ -7,17 +8,23 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'Date Mate';
-  users: any;
+ 
 
-
+  title = 'Dating';
+  users: any; 
+  
   constructor(private http: HttpClient) {}
   ngOnInit(): void {
     this.http.get('https://localhost:5001/api/users').subscribe({
-    next: response =>  this.users = response, 
+    next: response => this.users = response,
     error: error => console.log(error),
-    complete: () => console.log("Request has completed")
-  })
-  }
+    complete: () => console.log('Request has completed')
+
+    
   
+  })
+
+  setTheme('bs5');
+  }
+
 }
